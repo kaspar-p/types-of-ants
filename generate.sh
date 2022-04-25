4cbeccdc2ff59fc7b7181cb80e215fad15bcc5c4
Kaspar Poland <kaspar78@mouco.com>
2022-04-25 13:30:07 -0400

    Remove `ant` text after input bar

--- generate.sh
+++ generate.sh
@@ -1,12 +1,23 @@
 cp README.md README_OLD.md
+cp index.html index_OLD.html
 
 python3 generate/generate_page.py
 python3 generate/generate_readme.py
 
-diff_result=$(diff README.md README_OLD.md)
-if [[ "$diff_result" = "" ]]; then
-    echo "Nothing changed."
+diff_readme_result=$(diff README.md README_OLD.md)
+diff_index_result=$(diff index.html index_OLD.html)
+if [[ "$diff_readme_result" = "" ]]; then
+    if [[ "$diff_index_result" = "" ]]; then
+        echo "Nothing updated."
+    else
+        echo "index.html updated"
+    fi
 else
-    echo "Updated README.md and index.html page."
+    if [[ "$diff_index_result" = "" ]]; then
+        echo "README.md updated."
+    else
+        echo "Both index.html and README.md updated."
+    fi
 fi
-rm README_OLD.md
+
+rm README_OLD.md index_OLD.html
