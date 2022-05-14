b5cd28a9dc7f5739837c2a43f0c5f63d51b67d47
Kaspar Poland <kaspar78@mouco.com>
2022-05-14 17:24:01 -0400

    New ant release: banner

--- generate/generate_page.py
+++ generate/generate_page.py
@@ -10,7 +10,6 @@ def apply_ant_rule(ant: str) -> str:
     if "6krill" in ant:
         return '<a href="http://6krill.com">6krill ant</a>'
     if ant == "ant tm":
-        print(f"Ant is '{ant}'")
         return "ant&trade;"
 
     return ant
