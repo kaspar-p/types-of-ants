577f381e955e3df717671bf50200d2631da14d55
kaspar-p <kaspar78@mouco.com>
2023-06-10 19:03:09 -0600

    Server address

--- ant-on-the-web/server/src/main.rs
+++ ant-on-the-web/server/src/main.rs
@@ -1,10 +1,21 @@
 #[macro_use]
 extern crate rocket;
+use std::fs::File;
+use std::io::BufReader;
 
 #[rocket::get("/")]
 fn index() -> &'static str {
     return "Hello World!";
 }
+
+// #[rocket::get("/.well-known/acme-challenge/d7pvITjrjLvXFscDRJjvFJah00Gzas6SsvFLqn-UAQU")]
+// fn certs() -> &'static str {
+//     // let f: File = File::open("/etc/letsencrypt/live/beta.typesofants.key")?;
+//     let mut buf_reader = BufReader::new(f);
+//     let mut contents: String = String::new();
+//     buf_reader.read_to_string(&mut contents)?;
+//     return contents.as_str();
+// }
 
 #[launch]
 fn rocket() -> _ {
