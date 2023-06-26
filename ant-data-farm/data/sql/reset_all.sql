986d085bbedfdd0fb77cf06a863c4ba695644064
kaspar-p <kaspar78@mouco.com>
2023-06-26 16:28:18 -0600

    fix reset_all.sql script

--- ant-data-farm/data/sql/reset_all.sql
+++ ant-data-farm/data/sql/reset_all.sql
@@ -1,7 +1,7 @@
 -- Has to be run from ant-data-farm/ directory
 \i data/sql/bootstrap.sql;
-\i data/sql_output/ant.sql;
-\i data/sql_output/release.sql;
-\i data/sql_output/ant_release.sql;
-\i data/sql_output/ant_tweeted.sql;
-\i data/sql_output/ant_declined.sql;
+\i data/sql/ant.sql;
+\i data/sql/release.sql;
+\i data/sql/ant_release.sql;
+\i data/sql/ant_tweeted.sql;
+\i data/sql/ant_declined.sql;
