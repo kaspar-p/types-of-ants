d3f044d0d04020619141eb9517a166f8531a6d18
kaspar-p <kaspar78@mouco.com>
2023-06-27 22:20:21 -0600

    Add file logging

--- ant-on-twitter/src/main.rs
+++ ant-on-twitter/src/main.rs
@@ -1,14 +1,15 @@
-use std::time::Duration;
-
 use ant_data_farm::ants::Tweeted;
 use ant_data_farm::{ants::Ant, connect};
 use rand::seq::SliceRandom;
+use std::time::Duration;
 use tokio_cron_scheduler::{Job, JobScheduler};
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
 use twitter_v2::authorization::Oauth1aToken;
 use twitter_v2::TwitterApi;
 
 async fn post_tweet(ant_content: String) -> Option<twitter_v2::Tweet> {
-    println!("Tweeting with ant: {}", ant_content);
+    info!("Tweeting with ant: {}", ant_content);
 
     let consumer_key = dotenv::var("TWITTER_API_CONSUMER_KEY").unwrap();
     let consumer_secret = dotenv::var("TWITTER_API_CONSUMER_SECRET").unwrap();
@@ -33,8 +34,10 @@ async fn post_tweet(ant_content: String) -> Option<twitter_v2::Tweet> {
 }
 
 async fn cron_tweet() {
+    info!("Starting cron...");
     let dao = connect().await;
 
+    info!("Getting random ant choice...");
     let random_ant: Ant = {
         let read_ants = dao.ants.read().await;
         let ants = read_ants
@@ -49,11 +52,13 @@ async fn cron_tweet() {
             .to_owned()
     };
 
+    info!("Tweeting...");
     let res = post_tweet(random_ant.ant_name).await;
     if res.is_none() {
         panic!("Failed to tweet!");
     }
 
+    info!("Saving result to DB...");
     dao.ants
         .write()
         .await
@@ -63,12 +68,24 @@ async fn cron_tweet() {
 
 #[tokio::main]
 async fn main() {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .with_file(true)
+        .with_ansi(false)
+        .with_writer(tracing_appender::rolling::hourly(
+            "./logs/ant-on-twitter",
+            "ant-on-twitter.log",
+        ))
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
     dotenv::dotenv().unwrap_or_else(|e| panic!("Environment error: {}", e));
     let mut scheduler = JobScheduler::new().await.unwrap();
 
     scheduler
         .add(
-            Job::new_async("0 0 18 * * *", |_, __| {
+            Job::new_async("1/5 * * * * *", |_, __| {
                 Box::pin(async move {
                     cron_tweet().await;
                 })
