f8a766053ac3558781a252b3c74d87dbcc295aca
kaspar-p <kaspar78@mouco.com>
2023-11-09 14:09:57 -0500

    Timezone sensitivity

--- projects/ant-who-tweets/src/main.rs
+++ projects/ant-who-tweets/src/main.rs
@@ -1,5 +1,8 @@
+use ant_data_farm::ants::Ant;
 use ant_data_farm::ants::Tweeted;
-use ant_data_farm::{ants::Ant, connect};
+use ant_data_farm::{DatabaseConfig, DatabaseCredentials};
+use chrono::Timelike;
+use chrono::{DateTime, Local};
 use rand::seq::SliceRandom;
 use std::time::Duration;
 use tokio_cron_scheduler::{Job, JobScheduler};
@@ -8,19 +11,22 @@ use tracing_subscriber::FmtSubscriber;
 use twitter_v2::authorization::Oauth1aToken;
 use twitter_v2::TwitterApi;
 
-async fn post_tweet(ant_content: String) -> Option<twitter_v2::Tweet> {
+#[derive(Debug, Clone)]
+struct TwitterCredentials {
+    pub consumer_key: String,
+    pub consumer_secret: String,
+    pub access_token: String,
+    pub access_token_secret: String,
+}
+
+async fn post_tweet(ant_content: String, creds: TwitterCredentials) -> Option<twitter_v2::Tweet> {
     info!("Tweeting with ant: {}", ant_content);
 
-    let consumer_key = dotenv::var("TWITTER_API_CONSUMER_KEY").unwrap();
-    let consumer_secret = dotenv::var("TWITTER_API_CONSUMER_SECRET").unwrap();
-    let access_token = dotenv::var("TWITTER_API_ACCESS_TOKEN").unwrap();
-    let access_token_secret = dotenv::var("TWITTER_API_ACCESS_TOKEN_SECRET").unwrap();
-
     let token = Oauth1aToken::new(
-        consumer_key,
-        consumer_secret,
-        access_token,
-        access_token_secret,
+        creds.consumer_key,
+        creds.consumer_secret,
+        creds.access_token,
+        creds.access_token_secret,
     );
 
     let client = TwitterApi::new(token);
@@ -33,9 +39,10 @@ async fn post_tweet(ant_content: String) -> Option<twitter_v2::Tweet> {
         .into_data()
 }
 
-async fn cron_tweet() {
+async fn cron_tweet() -> () {
     info!("Starting cron...");
-    let dao = connect().await;
+    let config: Config = get_config().expect("Getting config failed!");
+    let dao = ant_data_farm::connect_config(config.database).await;
 
     info!("Getting random ant choice...");
     let random_ant: Ant = {
@@ -53,7 +60,7 @@ async fn cron_tweet() {
     };
 
     info!("Tweeting...");
-    let res = post_tweet(random_ant.ant_name).await;
+    let res = post_tweet(random_ant.ant_name, config.twitter).await;
     assert!(res.is_some(), "Failed to tweet!");
 
     info!("Saving result to DB...");
@@ -63,6 +70,43 @@ async fn cron_tweet() {
         .add_ant_tweet(&random_ant.ant_id)
         .await;
     info!("Cron tasks done, exiting...");
+}
+
+#[derive(Debug, Clone)]
+struct Config {
+    pub twitter: TwitterCredentials,
+    pub database: DatabaseConfig,
+}
+
+fn get_config() -> Result<Config, dotenv::Error> {
+    let twitter_env_key = "TWITTER_CREDS_FILE";
+    let db_env_key = "DATABASE_CREDS_FILE";
+
+    let twitter_env =
+        dotenv::var(twitter_env_key).expect("Twitter file credential not in environment!");
+    let db_env = dotenv::var(db_env_key).expect("Database file credentials not in environment!");
+    dotenv::from_path(twitter_env).expect("Twitter creds file not found!");
+    dotenv::from_path(db_env).expect("Database creds file not found!");
+
+    let config = Config {
+        twitter: TwitterCredentials {
+            consumer_key: dotenv::var("TWITTER_API_CONSUMER_KEY")?,
+            consumer_secret: dotenv::var("TWITTER_API_CONSUMER_SECRET")?,
+            access_token: dotenv::var("TWITTER_API_ACCESS_TOKEN")?,
+            access_token_secret: dotenv::var("TWITTER_API_ACCESS_TOKEN_SECRET")?,
+        },
+        database: DatabaseConfig {
+            creds: Some(DatabaseCredentials {
+                database_name: dotenv::var("DB_PG_NAME")?,
+                database_user: dotenv::var("DB_PG_USER")?,
+                database_password: dotenv::var("DB_PG_PASSWORD")?,
+            }),
+            host: Some(dotenv::var("DB_HOST")?),
+            port: Some(7000),
+        },
+    };
+    println!("Config: {:#?}", config);
+    return Ok(config);
 }
 
 #[tokio::main]
@@ -78,14 +122,16 @@ async fn main() {
         .finish();
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-
-    dotenv::dotenv().unwrap_or_else(|e| panic!("Environment error: {e}"));
     let mut scheduler = JobScheduler::new().await.unwrap();
+
+    let real_diff = chrono::offset::Utc::now().hour() - chrono::offset::Local::now().hour();
+    let expected_diff = 6;
+    let hour_offset = expected_diff - real_diff;
 
     scheduler
         .add(
             // 6pm MST is midnight UTC
-            Job::new_async("0 0 0 * * *", |_, __| {
+            Job::new_async(format!("0 0 {} * * *", hour_offset).as_str(), |_, __| {
                 Box::pin(async move {
                     cron_tweet().await;
                 })
