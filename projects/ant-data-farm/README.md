4998875a42d336fea2916feaadd7299fc5048d3a
kaspar-p <kaspar78@mouco.com>
2025-03-30 14:01:51 -0400

    Add updated_at and migration entry

---
+++ projects/ant-data-farm/README.md
@@ -0,0 +1,7 @@
+# `ant-data-farm`
+
+The database.
+
+## Developing
+
+See the [Developer Guide](../../docs/developer-guide.md).
--- projects/ant-data-farm/data/sql/016_new_migration_columns.sql
+++ projects/ant-data-farm/data/sql/016_new_migration_columns.sql
@@ -1,60 +1,88 @@
 BEGIN;
 
 alter table migration
-  add created_at timestamp with time zone -- When the migration was created.
+  add created_at timestamp with time zone, -- When the migration was created.
+  add updated_at timestamp with time zone -- When the migration was last updated.
 ;
 
 update migration
-set created_at = '2023-06-20 00:00:00'::timestamp with time zone
+set 
+  created_at = '2023-06-20 00:00:00'::timestamp with time zone,
+  updated_at = '2023-06-20 00:00:00'::timestamp with time zone
 where migration_label = 'Add phone number to the registered_user table'
 ;
 
 update migration
-set created_at = '2023-08-08 00:00:00'::timestamp with time zone
+set
+  created_at = '2023-08-08 00:00:00'::timestamp with time zone,
+  updated_at = '2023-08-08 00:00:00'::timestamp with time zone
 where migration_label = 'New ant release, 08-08-2023'
 ;
 
 update migration
-set created_at = '2024-02-03 00:00:00'::timestamp with time zone
+set
+  created_at = '2024-02-03 00:00:00'::timestamp with time zone,
+  updated_at = '2024-02-03 00:00:00'::timestamp with time zone
 where migration_label = 'New ant release, 02-03-2024'
 ;
 
 update migration
-set created_at = '2024-04-14 00:00:00'::timestamp with time zone
+set
+  created_at = '2024-04-14 00:00:00'::timestamp with time zone,
+  updated_at = '2024-04-14 00:00:00'::timestamp with time zone
 where migration_label = 'New ant release, 04-14-2024'
 ;
 
 update migration
-set created_at = '2024-05-05 00:00:00'::timestamp with time zone
+set
+  created_at = '2024-05-05 00:00:00'::timestamp with time zone,
+  updated_at = '2024-05-05 00:00:00'::timestamp with time zone
 where migration_label = 'Add the three hosts into host table, add more info into host table'
 ;
 
 update migration
-set created_at = '2024-05-05 00:00:00'::timestamp with time zone
+set
+  created_at = '2024-05-05 00:00:00'::timestamp with time zone,
+  updated_at = '2024-05-05 00:00:00'::timestamp with time zone
 where migration_label = 'Add the migration table'
 ;
 
 update migration
-set created_at = '2024-07-22 00:00:00'::timestamp with time zone
+set
+  created_at = '2024-07-22 00:00:00'::timestamp with time zone,
+  updated_at = '2024-07-22 00:00:00'::timestamp with time zone
 where migration_label = 'make migration label unique'
 ;
 
 update migration
-set created_at = '2024-07-22 00:00:00'::timestamp with time zone
+set
+  created_at = '2024-07-22 00:00:00'::timestamp with time zone,
+  updated_at = '2024-07-22 00:00:00'::timestamp with time zone
 where migration_label = 'make migration label unique'
 ;
 
 update migration
-set created_at = '2024-09-08 00:00:00'::timestamp with time zone
+set
+  created_at = '2024-09-08 00:00:00'::timestamp with time zone,
+  updated_at = '2024-09-08 00:00:00'::timestamp with time zone
 where migration_label = 'ant-release:2024.9.8'
 ;
 
 update migration
-set created_at = '2025-02-17 00:00:00'::timestamp with time zone
+set
+  created_at = '2025-02-17 00:00:00'::timestamp with time zone,
+  updated_at = '2025-02-17 00:00:00'::timestamp with time zone
 where migration_label = 'ant-release:2025.2.17'
 ;
 
 alter table migration
-alter column created_at set not null;
+alter column created_at set not null,
+alter column updated_at set not null
+;
+
+insert into migration (migration_label, created_at, updated_at)
+  values
+    ('new-migration-created-at-column', now(), now())
+;
 
 COMMIT;
